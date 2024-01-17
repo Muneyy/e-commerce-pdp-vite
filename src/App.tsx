96ea2bf96c25f532fb51295fac45effa9e10cc07
itsmeNEK <gasparkenken55@gmail.com>
2024-01-17 20:54:40 +0800

    -Carousel Container -Images -Next & Prev Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-import './testSass.sass';
+import ProductCarousel from './Components/ProductCarousel/ProductCarousel';
+import '@/assets/styles/globals.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React For Ecommerce-PDP</h1>
-      <div className="card">
-        <button type="submit" onClick={() => setCount((count) => count + 1)}>
-          count is
-          {' '}
-          {count}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="sass-class">
-        Hello
-      </p>
-    </>
+    <div className="wrapper">
+      <ProductCarousel />
+    </div>
   );
 }
 
