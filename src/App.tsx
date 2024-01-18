674543f8ec9b0c3e5dd3cecc62cf0efb746a8aa6
Muneyy <muneyyyy@gmail.com>
2024-01-18 16:12:55 +0800

    Add Cart Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import './App.css';
-import './testSass.sass';
 import Header from './Components/Header/Header';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       {/* <div>
