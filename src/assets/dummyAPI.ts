2395f08b9f9881aaa6537e88aeb5b67469add7db
Muneyy <muneyyyy@gmail.com>
2024-01-18 19:29:40 +0800

    create dummy object for product details

---
+++ src/assets/dummyAPI.ts
@@ -0,0 +1,10 @@
+const productDetails = {
+  id: 1,
+  company: 'Sneaker Company',
+  title: 'Fall Limited Edition Sneakers',
+  description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.',
+  price: 250,
+  discount: 0.5,
+};
+
+export { productDetails };
