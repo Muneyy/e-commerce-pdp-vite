6a477a668c9a24322bfe99ff729de805d4b58be9
itsmeNEK <gasparkenken55@gmail.com>
2024-01-19 21:37:08 +0800

    Product Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import ProductCarousel from './Components/ProductCarousel/ProductCarousel';
 import '@/assets/styles/globals.scss';
+import { useMediaQuery } from './Hooks/useMedia';
 
 function App() {
+  const isMobileView = useMediaQuery(375);
+
   return (
     <div className="wrapper">
-      <ProductCarousel />
+      {isMobileView ? (<ProductCarousel />) : (
+        <div>
+          <h1>Desktop View</h1>
+        </div>
+      )}
+
     </div>
   );
 }
