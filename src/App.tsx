aed28a35c9ae056900e820963571f73e9dab3f78
itsmeNEK <gasparkenken55@gmail.com>
2024-01-19 19:35:58 +0800

    -useMediaQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import ProductImages from './Components/ProductImages/ProductImages';
 import ProductCarousel from './Components/ProductCarousel/ProductCarousel';
 import '@/assets/styles/globals.scss';
+import useMediaQuery from './Hooks/useMedia';
 
 function App() {
+  const isMobileView = useMediaQuery(375);
   return (
-    <div className="wrapper">
-      <ProductCarousel />
-      <ProductImages />
+    <div>
+      {isMobileView ? (
+        <ProductCarousel />
+      ) : (
+        <div className="wrapper">
+          <ProductImages />
+        </div>
+      )}
     </div>
+
   );
 }
 
---
+++ src/Hooks/useMedia.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect, useCallback } from 'react';
+
+const useMediaQuery = (width: number) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e: MediaQueryListEvent) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addListener(updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeListener(updateTarget);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [width, updateTarget]);
+
+  return targetReached;
+};
+
+export default useMediaQuery;
