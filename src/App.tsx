6557a2eb9c74c1ad61beb7aebfc167f66c4a1c27
itsmeNEK <gasparkenken55@gmail.com>
2024-01-19 19:18:29 +0800

    -Product Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import Header from './Components/Header/Header';
 import ProductDetails from './Components/ProductDetails/ProductDetails';
 import ProductImages from './Components/ProductImages/ProductImages';
+import ProductCarousel from './Components/ProductCarousel/ProductCarousel';
 import '@/assets/styles/globals.scss';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
 
   return (
     <div className="wrapper">
+      <ProductCarousel />
       <ProductImages />
       <Header />
       {isLoaded ? (
