c94174edf3df2b7d46cd977d13f49dd2dcca54d9
Muneyy <muneyyyy@gmail.com>
2024-01-23 22:41:11 +0800

    Remove base for vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://muneyy.github.io/e-commerce-pdp-vite/",
   resolve: {
     alias: {
       "@": "/src",
